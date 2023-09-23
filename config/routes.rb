0a513d6fa9a1d6f53c4e4ef50ee3f1cf3f3ef966
Zurab Khadikov <zurab.khadikov@endress.com>
2023-09-23 16:11:52 +0200

    change routes to use /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts
+  # API routes should be in /api/v1 namespace
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
