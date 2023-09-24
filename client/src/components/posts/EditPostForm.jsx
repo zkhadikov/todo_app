8c00b0bca2d6045c2566ecbb7cade9dd77b301ab
Zurab Khadikov <zurab.khadikov@endress.com>
2023-09-24 16:40:57 +0200

    added edit functionality

---
+++ client/src/components/posts/EditPostForm.jsx
@@ -0,0 +1,82 @@
+import React, {useEffect, useState} from "react";
+import {useParams, useNavigate} from "react-router-dom";
+import {API_URL} from "../../constants.js";
+
+function EditPostForm() {
+  const [post, setPost] = useState(null);
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const fetchCurrentPost = async () => {
+    try {
+      const response = await fetch(`${API_URL}/${id}`);
+      if (response.ok) {
+        const json = await response.json();
+        setPost(json);
+      } else {
+        throw new Error("Something went wrong");
+      }
+    } catch (error) {
+      console.log(error);
+      setPost(null);
+    }
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const payload = {title: post.title, body: post.body};
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(payload),
+      } );
+      if (response.ok) {
+        const json = await response.json();
+        navigate(`/posts/${json.id}`);
+      } else {
+        throw new Error("Something went wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCurrentPost();
+  }, [id]);
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>Edit Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="post-title">Title:</label>
+          <input
+            id="post-title"
+            type="text"
+            value={post.title}
+            onChange={(e) => setPost({...post, title: e.target.value})}
+          />
+        </div>
+        <div>
+          <label htmlFor="post-body">Body:</label>
+          <textarea
+            id="post-body"
+            value={post.body}
+            onChange={(e) => setPost({...post, body: e.target.value})}
+          />
+        </div>
+        <div>
+          <button type="submitt">Update Post</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default EditPostForm;
--- client/src/components/routes/AppRoutes.jsx
+++ client/src/components/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import PostsList from "../posts/PostsList";
 import PostDetails from "../posts/PostDetails";
 import NewPostForm from "../posts/NewPostForm";
+import EditPostForm from "../posts/EditPostForm.jsx";
 
 function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ function AppRoutes() {
       <Route path="/" element={<PostsList />} />
       <Route path="/posts" element={<PostsList />} />
       <Route path="/posts/:id" element={<PostDetails/>} />
+      <Route path="/posts/:id/edit" element={<EditPostForm/>} />
       <Route path="/posts/new" element={<NewPostForm/>} />
     </Routes>
   );
