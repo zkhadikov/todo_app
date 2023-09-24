2c7239dae0a8c93f66554510b86be6979abd9faa
Zurab Khadikov <zurab.khadikov@endress.com>
2023-09-24 10:14:41 +0200

    add new poast form

--- client/src/components/routes/AppRoutes.jsx
+++ client/src/components/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PostsList from "../posts/PostsList";
 import PostDetails from "../posts/PostDetails";
+import NewPostForm from "../posts/NewPostForm";
 
 function AppRoutes() {
   return (
@@ -9,7 +10,7 @@ function AppRoutes() {
       <Route path="/" element={<PostsList />} />
       <Route path="/posts" element={<PostsList />} />
       <Route path="/posts/:id" element={<PostDetails/>} />
-      <Route path="/posts/new" element={<h1>New Post</h1>} />
+      <Route path="/posts/new" element={<NewPostForm/>} />
     </Routes>
   );
 }
